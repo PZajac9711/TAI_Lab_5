02d76c21c9d77eb3ac0cd45c63b83ad68fff0be0
PZajac9711 <patzaj9711@gmail.com>
2020-04-07 11:00:10 +0200

    finished lab6

--- blog/src/app/shared/search-bar/search-bar.component.ts
+++ blog/src/app/shared/search-bar/search-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit,EventEmitter,Output } from '@angular/core';
-
+import {Router, ActivatedRoute} from '@angular/router';
 @Component({
   selector: 'search-bar',
   templateUrl: './search-bar.component.html',
@@ -9,13 +9,20 @@ export class SearchBarComponent implements OnInit {
   filterText: string;
   @Output() name = new EventEmitter<string>();
 
-  constructor() {
+  constructor(private router: Router, private route: ActivatedRoute) {
   }
 
   ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.filterText = params['name'];
+      this.sendFilter();
+    });
+
   }
 
   sendFilter() {
+    this.router.navigate(['/blog'], {queryParams: {name:
+        this.filterText}});
     this.name.emit(this.filterText);
   }
 
