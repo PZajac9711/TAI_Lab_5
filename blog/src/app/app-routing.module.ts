5146acc1649249a956e6e0cb12487874873553b9
PZajac9711 <patzaj9711@gmail.com>
2020-05-11 14:06:02 +0200

    added task 6

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import {ContactComponent} from "./components/contact/contact.component";
 import {BlogComponent} from "./components/blog/blog.component";
 import {BlogItemDetailsComponent} from "./components/blog-item-details/blog-item-details.component";
 import {BlogHomeComponent} from "./components/blog-home/blog-home.component";
+import {AddPostComponent} from "./components/add-post/add-post.component";
 
 
 const routes: Routes = [{
@@ -22,6 +23,10 @@ const routes: Routes = [{
   {
     path: 'blog/detail/:id',
     component: BlogItemDetailsComponent
+  },
+  {
+    path: 'add-post',
+    component: AddPostComponent
   },
 
 ];
